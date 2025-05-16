36a98578c375be1c6f91b235b8b7552116361d94
ba-chaudoir <Basile.Chaudoir@uliege.be>
2025-05-16 11:10:34 +0200

    Solar modif

--- labothappy/component/solar/parabolic_trough/parabolicplant_des.py
+++
@@ -1,8 +0,0 @@
-import __init__
-import numpy as np
-import component.solar.parabolic_trough.parabolictroughcollector as parabolictroughcollector
-from geometries.solar.parabolictrough_geometry import PT_Collector_Geom
-
-PT_geom = PT_Collector_Geom()
-PT_geom.set_parameters("Soponova_MicroCSP")
-
