edf392101e154a98f8fe3c035c3502aab322c3d9
Elise Neven <155994101+el-neven@users.noreply.github.com>
2025-11-21 16:20:21 +0100

    HP_example

--- labothappy/component/heat_exchanger/hex_csteff.py
+++ labothappy/component/heat_exchanger/hex_csteff.py
@@ -88,6 +88,8 @@ class HexCstEff(BaseComponent):
         # Return a list of required inputs
         return['fluid_C', 'h_su_C', 'P_su_C', 'm_dot_C', 'fluid_H', 'h_su_H', 'P_su_H', 'm_dot_H']
     
+    def get_required_parameters(self):
+        return ['eta']
 
     def solve(self):
         # Ensure all required checks are performed
