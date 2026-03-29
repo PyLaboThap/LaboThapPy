4fec0918b61ad5a988a12cc8204740d2637bee04
TitouanJanod <titouan.janod@uliege.be>
2026-03-29 18:45:51 +0200

    Starting to add ACC for tests

--- labothappy/component/examples/heat_exchanger/hexs_MB_Zorlu.py
+++ labothappy/component/examples/heat_exchanger/hexs_MB_Zorlu.py
@@ -22,7 +22,7 @@ from toolbox.geometries.heat_exchanger.c_geometry_HXs_Zorlu import Zorlu_HXs
 
 import numpy as np
 
-HX_name = 'RECUPERATOR'
+HX_name = 'PREHEATER'
 
 n_disc =30
     
@@ -91,7 +91,7 @@ elif HX_name == "RECUPERATOR":
     
         # Second fluid
         fluid_C = 'Cyclopentane',
-        T_su_C = 62.71 + 273.15, # K
+        T_su_C = 31.27 + 273.15, # K
         P_su_C = 8.258*1e5, # Pa
         m_dot_C = 62.74, # kg/s  
         )
@@ -118,9 +118,53 @@ elif HX_name == "RECUPERATOR":
     HX.solve() 
     HX.print_states_connectors()
     
+    epsilon_rec = HX.Q_dot / (HX.su_C.m_dot * HX.su_C.cp * (HX.su_H.T - HX.su_C.T))
+    print(f'  - epsilon_rec = {epsilon_rec}')
     # "Verification of epsilon"
     # C_dot_C = HX.su_C.m_dot * HX.su_C.cp
     # C_dot_H = HX.su_H.m_dot * HX.su_H.cp
     # C_dot_min = min(C_dot_C , C_dot_H)
     # epsilon_rec = HX.Q_dot / (C_dot_min  * (HX.su_H.T - HX.su_C.T))
     # print(f'  - epsilon = {epsilon_rec}')
+    
+elif HX_name == "ACC":
+    HX = HexMBChargeSensitive('Tube&Fins')
+    
+    HX.set_inputs(
+        fluid_H = 'Cyclopentane',
+        T_su_H = 64.4 + 273.15, # K
+        P_su_H = 0.8*1e5, # Pa
+        m_dot_H = 62.71, # kg/s
+    
+        # Second fluid
+        fluid_C = 'Air',
+        T_su_C = 31.27 + 273.15, # K
+        P_su_C = 8.258*1e5, # Pa
+        m_dot_C = 62.74, # kg/s  
+        )
+    "Set HTC"
+    Corr_C = {"1P" : "Gnielinski", "2P" : "Boiling_curve"}
+    Corr_H = {"1P" : "Tube_And_Fins", "2P" : "Tube_And_Fins"}
+    # Corr_H = {"1P" : "Tube&Fins", "2P" : "Tube&Fins"}
+    HX.set_htc(htc_type = 'Correlation', Corr_H = Corr_H, Corr_C = Corr_C) # 'User-Defined' or 'Correlation'
+    
+    
+    "Set geometry"
+    geom_obj = Zorlu_HXs()
+    geom_obj.set_parameters("ORC_ACC")
+    HX.set_parameters(**geom_obj.geom)
+    HX.set_parameters(n_disc=n_disc)
+    
+    "Set pressure drops"
+    # geom_obj.set_DP("ORC_recuperator")
+    # HX.set_DP(**geom_obj.DP) 
+    HX.set_DP() # no pressure drops normally
+        
+    "Solve the component"
+    
+    HX.solve() 
+    HX.print_states_connectors()
+    
+    epsilon_rec = HX.Q_dot / (HX.su_C.m_dot * HX.su_C.cp * (HX.su_H.T - HX.su_C.T))
+    print(f'  - epsilon_ACC = {epsilon_ACC}')
+    
