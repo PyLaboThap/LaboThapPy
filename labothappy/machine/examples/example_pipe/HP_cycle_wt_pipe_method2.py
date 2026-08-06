8af452dcf525a89c11f70bb165744eaf18cd74d2
Elise Neven <elise.neven@uliege.be>
2026-08-06 14:02:55 +0200

    Second example for pipe introduction

---
+++ labothappy/machine/examples/example_pipe/HP_cycle_wt_pipe_method2.py
@@ -0,0 +1,125 @@
+from labothappy.machine.circuit_it import IterativeCircuit
+from labothappy.connector.mass_connector import MassConnector
+from labothappy.component.compressor.compressor_csteff import CompressorCstEff
+from labothappy.component.heat_exchanger.hex_cstpinch import HexCstPinch
+from labothappy.component.valve.valve_isenthalpic import ValveIsenthalpic
+from labothappy.component.pipe.pipe import Pipe
+
+from CoolProp.CoolProp import PropsSI
+import numpy as np
+
+# Instanciate Circuit
+fluid = "Propane"
+HP = IterativeCircuit(fluid)
+
+# Create components
+Compressor = CompressorCstEff()
+Pipe_Compressor_to_Condenser = Pipe()
+Condenser = HexCstPinch()
+Pipe_Condenser_to_ExpansionValve = Pipe()
+ExpansionValve = ValveIsenthalpic()
+Pipe_ExpansionValve_to_Evaporator = Pipe()
+Evaporator = HexCstPinch()
+Pipe_Evaporator_to_Compressor = Pipe()
+
+# Set component parameters
+eta_is_cp = 0.8
+Pinch_cd = 3  # K
+SC_cd = 3  # K
+Pinch_ev = 3  # K
+SH_ev = 3  # K
+
+# Component parameters
+Compressor.set_parameters(eta_is=eta_is_cp)
+Condenser.set_parameters(Pinch=Pinch_cd, Delta_T_sh_sc=SC_cd, HX_type="condenser")
+Evaporator.set_parameters(Pinch=Pinch_ev, Delta_T_sh_sc=SH_ev, HX_type="evaporator")
+
+# Pipes geometries
+Pipe_Compressor_to_Condenser.add_straight(D=0.012, L=1.0, theta = 0).add_curved_elbow(D=0.012, delta=90, R0_D=1.5).add_straight(D=0.012, L=0.5, theta = 90) # 10mm diameter, 1.5m long
+Pipe_Condenser_to_ExpansionValve.add_straight(D=0.012, L=1.0, theta = 0)
+Pipe_ExpansionValve_to_Evaporator.add_curved_elbow(D=0.012, delta=90, R0_D=1.5)
+Pipe_Evaporator_to_Compressor.add_straight(D=0.012, L=1.0, theta = 0)
+
+
+# Add components to circuit
+HP.add_component(Compressor, "Compressor")
+HP.add_component(Condenser, "Condenser")
+HP.add_component(ExpansionValve, "ExpansionValve")
+HP.add_component(Evaporator, "Evaporator")
+
+# With pipe routing
+HP.link_components("Compressor", "m-ex", "Condenser", "m-su_H")
+
+HP.link_components("Condenser", "m-ex_H", "ExpansionValve", "m-su")
+
+HP.link_components("ExpansionValve", "m-ex", "Evaporator", "m-su_C")
+
+HP.link_components("Evaporator", "m-ex_C", "Compressor", "m-su")
+
+
+# Add fluid sources
+CD_source = MassConnector('Water')
+T_su_w_cd = 40+273.15
+P_su_w_cd = 3e5
+m_dot_w_cd = 0.5  # kg/s
+EV_source = MassConnector('Water')
+T_su_w_ev = 20+273.15
+P_su_w_ev = 1e5
+m_dot_w_ev = 5  # kg/s
+
+HP.add_source("CD_Water", CD_source, HP.components["Condenser"], "m-su_C")
+HP.set_source_properties(T=T_su_w_cd, fluid='Water', P=P_su_w_cd, m_dot = m_dot_w_cd, target="CD_Water")
+HP.add_source("EV_Water", EV_source, HP.components["Evaporator"], "m-su_H")
+HP.set_source_properties(T=T_su_w_ev, fluid='Water', P=P_su_w_ev, m_dot = m_dot_w_ev, target="EV_Water")
+
+HP.add_source("CD_Water", CD_source, HP.components["Condenser"], "m-su_C")
+HP.set_source_properties(T=T_su_w_cd, fluid='Water', P=P_su_w_cd, m_dot = m_dot_w_cd, target="CD_Water")
+HP.add_source("EV_Water", EV_source, HP.components["Evaporator"], "m-su_H")
+HP.set_source_properties(T=T_su_w_ev, fluid='Water', P=P_su_w_ev, m_dot = m_dot_w_ev, target="EV_Water")
+
+
+# Set cycle inputs
+m_dot_ref = 0.2 # kg/s
+SC_cd = 3  # K
+SH_ev = 3  # K
+
+HP.set_cycle_input(target="ExpansionValve:su", m_dot = m_dot_ref, SC=SC_cd)
+HP.set_cycle_input(target="Evaporator:ex_C", SH=SH_ev)
+
+# Set iteration variables
+P_HP_guess = PropsSI("P", "T", T_su_w_cd+10, "Q", 0, fluid)
+P_LP_guess = PropsSI("P", "T", T_su_w_ev-10, "Q", 1, fluid)
+
+HP.set_iteration_variable(
+    target=["ExpansionValve:su", "Compressor:ex"],
+    variable="p",
+    guess=P_HP_guess,
+    tolerance=1e-6
+)
+
+HP.set_iteration_variable(
+    target="ExpansionValve:ex",
+    variable="p",
+    guess=P_LP_guess,
+    tolerance=1e-6
+)
+
+# Set residual variables
+HP.set_residual_variable(
+    pre_target="ExpansionValve:su",
+    post_target="Condenser:ex_H",
+    variable="p",
+    tolerance=1e-3
+)
+
+HP.set_residual_variable(
+    pre_target="ExpansionValve:ex",
+    post_target="Evaporator:su_C",
+    variable="p",
+    tolerance=1e-3
+)
+
+# Solve circuit
+HP.solve()
+print(f"Converged at P_HP = {Compressor.ex.p}, P_LP = {Compressor.su.p}")
+# HP.print_states()
